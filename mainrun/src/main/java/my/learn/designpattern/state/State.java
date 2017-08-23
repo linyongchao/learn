eacaf891e6b0901793fade6e1da800aac41506b9
linyongchao <lycwork@126.com>
2017-08-23 15:46:53 +0800

    add:设计模式学习之状态模式

---
+++ mainrun/src/main/java/my/learn/designpattern/state/State.java
@@ -0,0 +1,22 @@
+package my.learn.designpattern.state;
+
+public class State {
+	private String value;
+
+	public void method1() {
+		System.out.println("execute the first opt!");
+	}
+
+	public void method2() {
+		System.out.println("execute the second opt!");
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
